656400fd45947a1f9fbc189dd02f36f48d25753d
andreafrancioni <andreafrancioni.work@gmail.com>
2023-11-13 17:13:31 +0100

    Modificati file originali ed importato precedente esercizio carosello

--- js/main.js
+++ js/main.js
@@ -1,24 +1,41 @@
-const images = [
-    {
-        image: 'img/01.webp',
-        title: 'Marvel\'s Spiderman Miles Morale',
-        text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
-    }, {
-        image: 'img/02.webp',
-        title: 'Ratchet & Clank: Rift Apart',
-        text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
-    }, {
-        image: 'img/03.webp',
-        title: 'Fortnite',
-        text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
-    }, {
-        image: 'img/04.webp',
-        title: 'Stray',
-        text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
-    }, {
-        image: 'img/05.webp',
-        title: "Marvel's Avengers",
-        text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
-    }
+imagesPath = [
+                `../img/01.webp`,
+                `../img/02.webp`,
+                `../img/03.webp`,
+                `../img/04.webp`,
+                `../img/05.webp`,
 ];
 
+let immaginiDaInserire = "";
+for (let i = 0; i < imagesPath.length; i++) {
+    const percorsoImmagine = imagesPath[i];
+    immaginiDaInserire += `<img src="${percorsoImmagine}">`;
+}
+document.getElementById("images").innerHTML = immaginiDaInserire;
+
+let currentImg = 0;
+const images = document.querySelectorAll(".images img");
+images[currentImg].classList.add("active");
+
+document.getElementById("btn-next").addEventListener("click", function () {
+    console.log("avanti");
+    if (currentImg < images.length - 1) {
+        images[currentImg].classList.remove("active");
+        currentImg++;
+        images[currentImg].classList.add("active");
+    }
+    if(currentImg == images.length - 1){
+        images[currentImg].classList.add("active");
+        currentImg=0;
+
+    }
+});
+
+document.getElementById("btn-back").addEventListener("click", function () {
+    console.log("indietro");
+    if (currentImg > 0) {
+        images[currentImg].classList.remove("active");
+        currentImg--;
+        images[currentImg].classList.add("active");
+    }
+});
